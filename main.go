5db535203e2f5c541e50d36808099b7f76e65095
Beknur Karshyga <beknur.karshyga@kassa24.kz@ipanb0027.ipa.kassa24.kz>
2022-03-17 11:31:43 +0600

    images added

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 	"io/ioutil"
 	"log"
@@ -22,25 +24,51 @@ func main() {
 	r := Routers()
 	a := app.New()
 	window := a.NewWindow("Smart Mirror")
+	// Main menu
+	fileMenu := fyne.NewMenu("File",
+		fyne.NewMenuItem("Quit", func() { a.Quit() }),
+	)
 
+	helpMenu := fyne.NewMenu("Help",
+		fyne.NewMenuItem("About", func() {
+			dialog.ShowCustom("About", "Close", container.NewVBox(
+				widget.NewLabel("Welcome to Gopher, a simple Desktop app created in Go with Fyne."),
+				widget.NewLabel("Version: v0.1"),
+				widget.NewLabel("Author: Aurélie Vache"),
+			), window)
+		}))
+	mainMenu := fyne.NewMainMenu(
+		fileMenu,
+		helpMenu,
+	)
+	window.Resize(fyne.Size{500, 500})
+	window.SetMainMenu(mainMenu)
 	hello = widget.NewLabel("hello beka")
-	res, err := fyne.LoadResourceFromPath("../Загрузки/logo.png")
+	res, err := fyne.LoadResourceFromPath("images/logo.png")
 	if err != nil {
 		log.Fatalln(err)
 	}
+	theme, err := fyne.LoadResourceFromPath("images/theme.jpeg")
+	themeImg := canvas.NewImageFromResource(theme)
+	themeImg.SetMinSize(fyne.Size{500, 500})
 	window.SetIcon(res)
 	window.SetContent(container.NewVBox(
 		hello,
+		themeImg,
 		widget.NewButton("get", func() {
 			hello.SetText("Hi there")
+			a.SendNotification(fyne.NewNotification("Обновление", "у вас новое сообщение"))
+		}),
+		widget.NewButton("Обновить данные?", func() {
+			hello.SetText("")
 		}),
 	))
+	hello.Move(fyne.NewPos(-200, 400))
 	go func() {
 		log.Println("server started")
 		log.Fatalln(http.ListenAndServe(":8080", r))
 	}()
 	window.ShowAndRun()
-
 }
 
 func Routers() *http.ServeMux {
@@ -61,6 +89,13 @@ func Main(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Println(t.Text)
-	w.Write([]byte("vse ok"))
+	m := map[string]string{
+		"status":    "ok",
+		"error":     "",
+		"deqystvie": "текст изменен",
+	}
+
+	data, _ := json.Marshal(m)
+	w.Write(data)
 	hello.SetText(t.Text)
 }
